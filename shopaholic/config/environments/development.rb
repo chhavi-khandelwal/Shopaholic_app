745f4b82529076c324342d694720eb0b87d8e0e3
Chhavi Khandelwal <chhavi@vinsol.com>
2014-02-06 19:57:52 +0530

    file added

--- shopaholic/config/environments/development.rb
+++ shopaholic/config/environments/development.rb
@@ -26,4 +26,5 @@ Shopaholic::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
 end
