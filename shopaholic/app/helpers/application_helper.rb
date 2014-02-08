fc0470b95235c480a69663224c3d9d622499a026
Chhavi Khandelwal <chhavi@vinsol.com>
2014-02-08 18:46:10 +0530

    helper method for image attachmnt added

--- shopaholic/app/helpers/application_helper.rb
+++ shopaholic/app/helpers/application_helper.rb
@@ -1,9 +1,16 @@
 module ApplicationHelper
-  def list_by(selectors)
-    selection_list = []
-    selectors.each do |selector|
-      selection_list.push([selector.name, selector.id])
-    end
-    selection_list
+
+  def link_to_remove_fields(name)
+    link_to name, 'javascript:;', class: 'remove-image'
   end
+  
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to name, 'javascript:;', "data-association" => "#{association}", "data-content" => "#{fields}", "class" => 'add-image'
+  end
+  
 end
+
