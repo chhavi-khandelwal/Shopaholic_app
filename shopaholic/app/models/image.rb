e95c38677823a6f9bf39f3c3dd08280a12a445b2
Chhavi Khandelwal <chhavi@vinsol.com>
2014-02-08 18:46:49 +0530

    comments removed

--- shopaholic/app/models/image.rb
+++ shopaholic/app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :display_pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :display_pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpg"
   
   # validates_attachment_content_type :display_pic, :content_type => /\Aimage\/.*\Z/
 end
