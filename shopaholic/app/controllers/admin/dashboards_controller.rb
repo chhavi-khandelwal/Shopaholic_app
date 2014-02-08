72bb6132bb95ee05b96dbe87c5363d4fe6f7a739
Chhavi Khandelwal <chhavi@vinsol.com>
2014-02-08 18:45:30 +0530

    show action modified

--- shopaholic/app/controllers/admin/dashboards_controller.rb
+++ shopaholic/app/controllers/admin/dashboards_controller.rb
@@ -3,5 +3,6 @@ class Admin::DashboardsController < ApplicationController
     #FIXME_AB: Use pagination
     @products = Product.all
     @categories = Category.all
+    @recent_products = @products.order('created_at DESC')
   end
 end
