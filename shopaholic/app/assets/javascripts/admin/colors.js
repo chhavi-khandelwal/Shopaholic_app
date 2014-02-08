7d9f01ec230ad40317c150b2fe78eb832380afdc
Chhavi Khandelwal <chhavi@vinsol.com>
2014-02-08 18:40:39 +0530

    js for paperclip images added

---
+++ shopaholic/app/assets/javascripts/admin/colors.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  var imageHelper = new ImageHelper();
+  imageHelper.bindEvents();
+});
+  
+function ImageHelper() {
+  var imageHelper = this;
+  var imageCount = 3;
+
+  this.bindEvents = function() {
+    $('.color-table').on('click', '.add-image', imageHelper.addField);
+    $('.color-table').on('click', '.remove-image', imageHelper.removeField);
+  }
+
+  this.addField = function() {
+    var $link = $(this);
+    var visibleImages = $('.color-table .image-field:visible');
+    if (visibleImages.size() < imageCount) {
+      var new_id = new Date().getTime();
+      var regexp = new RegExp("new_" + $link.data('association'), "g");
+      $link.parent().before($link.data('content').replace(regexp, new_id));
+    };
+
+  }
+
+  this.removeField = function() {
+    var $link = $(this);
+    $link.prev("input[type='hidden']").val("1");
+    $link.closest('span.image-field').hide();
+  }
+}
