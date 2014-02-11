e344e430caad5a6247dfd24e27921b317dd3ac43
Chhavi Khandelwal <chhavi@vinsol.com>
2014-02-11 20:34:00 +0530

    sku field added

--- shopaholic/db/migrate/20140130074111_create_sizes.rb
+++ shopaholic/db/migrate/20140130074111_create_sizes.rb
@@ -2,6 +2,7 @@ class CreateSizes < ActiveRecord::Migration
   def change
     create_table :sizes do |t|
       t.string :name
+      t.string :sku
       t.decimal :price, precision:10, scale: 2
       t.decimal :discounted_price, precision:10, scale:2
       t.integer :quantity
