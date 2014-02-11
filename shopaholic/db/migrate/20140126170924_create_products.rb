9df68cdb358ddca85283c75faf022ea229dc70e8
Chhavi Khandelwal <chhavi@vinsol.com>
2014-02-11 20:33:15 +0530

    description type changed to text

--- shopaholic/db/migrate/20140126170924_create_products.rb
+++ shopaholic/db/migrate/20140126170924_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :title
-      t.string :description
+      t.text :description
       t.references :category, index: true
       t.references :brand, index: true
       t.timestamps
