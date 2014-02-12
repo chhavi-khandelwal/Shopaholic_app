f267a82639a9265fb4388c8ef35292fb6a0cd545
Chhavi Khandelwal <chhavi@vinsol.com>
2014-02-12 12:23:11 +0530

    css added

--- shopaholic/app/assets/javascripts/admin/products.js
+++ shopaholic/app/assets/javascripts/admin/products.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  
-})
+  var colorFormHelper = new ColorFormHelper();
+  colorFormHelper.bindEvents();
+});
+
+function ColorFormHelper() {
+  var colorFormHelper = this;
+  this.bindEvents = function() {
+    // $('.product-desc').on('click', '#create-color', this.submit);
+  }
+
+  this.submitForm = function(e) {
+    alert('abc')
+  }
+}
