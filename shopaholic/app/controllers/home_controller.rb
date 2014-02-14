5927e46dd0a00f5b818885726411f5460e03189d
Chhavi Khandelwal <chhavi@vinsol.com>
2014-02-14 14:10:40 +0530

    code cleaned

--- shopaholic/app/controllers/home_controller.rb
+++ shopaholic/app/controllers/home_controller.rb
@@ -3,24 +3,8 @@ class HomeController < ApplicationController
   before_action :get_products, only: [:get_products_data]
   
   def index
-    @products = Product.all.order('created_at').limit(9)
+    @products = Product.all.order(:created_at).limit(9)
   end
 
-  def get_products_data
-    respond_to do |format|
-      format.json { render json: @products.to_json }
-    end
-  end
-
-  private
-  def get_products
-    @category = Category.find(params[:id]) 
-    @products = @category.products
-    # @product_images = @category.products.map do |product|
-    #   color = product.colors.first;
-    #   if color
-    #     color.images.first.file.url(:medium)
-    #   end
-    # end
-  end
+  
 end
